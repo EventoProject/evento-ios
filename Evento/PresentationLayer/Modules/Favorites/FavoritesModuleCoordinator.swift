50e6cfe2254412bebca5e7496850823710f33271
YeskendirAyat <a_eskendir08@mail.ru>
2023-06-13 02:57:36 +0600

    transitions are completed

--- Evento/PresentationLayer/Modules/Favorites/FavoritesModuleCoordinator.swift
+++ Evento/PresentationLayer/Modules/Favorites/FavoritesModuleCoordinator.swift
@@ -29,9 +29,37 @@ private extension FavoritesModuleCoordinator {
             self?.showEventPage(eventId: eventId)
         }
         
+        viewModel.showUserProfilePage = { [weak self] userID in
+            self?.showUserProfile(id: userID)
+        }
+        
+        
         let page = UIHostingController(rootView: LikedEventsPage(viewModel: viewModel))
         page.title = "Liked"
         router.set(viewControllers: [page], animated: true)
+    }
+    func showUserProfile(id: Int){
+        let viewModel = UserProfileViewModel(apiManager: injection.inject(ProfileApiManagerProtocol.self),eventApiManager: injection.inject(EventsApiManagerProtocol.self), chatApiManager: injection.inject(ChatApiManagerProtocol.self), id: id)
+        viewModel.showChatPage = { [weak self] room in
+            self?.showChatPage(chatId: room.roomID, otherUserName: room.username)
+        }
+        viewModel.showEventDetailPage={[weak self] eventId in
+            self?.showEventPage(eventId: eventId)
+        }
+        let page = UIHostingController(rootView: UserProfilePage(viewModel: viewModel))
+        router.push(viewController: page, animated: true)
+    }
+    
+    func showChatPage(chatId: String, otherUserName: String) {
+        let viewModel = ChatViewModel(
+            chatId: chatId,
+            apiManager: injection.inject(ChatApiManagerProtocol.self),
+            keychainManager:  injection.inject(KeychainManagerProtocol.self),
+            websocketManager: injection.inject(WebSocketManagerProtocol.self)
+        )
+        let page = UIHostingController(rootView: ChatPage(viewModel: viewModel))
+        page.title = otherUserName
+        router.push(viewController: page, animated: true)
     }
     
     func showEventPage(eventId: Int) {
